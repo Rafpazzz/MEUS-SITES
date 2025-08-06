0669fa022f5846f43e6866eae9d67be2ec243571
Rafael Paz <rafael_mourapaz@hotmail.com>
2025-08-06 12:55:35 -0300

    swith case

---
+++ Ruby/aula04.rb
@@ -0,0 +1,19 @@
+grade = 'A'
+
+# case grade
+# when 'A'
+#   puts "Aprovado"
+# when 'D'
+#   puts "Prova Final"
+# when 'F'
+#   puts "Reprovado"
+# else
+#   puts "Valor invalido"
+# end
+
+case grade
+when 'A' then puts "Aprovado"
+when 'D' then puts "prova Final"
+when 'F' then puts "Reprovado"
+else puts "Valor invalido"
+end
