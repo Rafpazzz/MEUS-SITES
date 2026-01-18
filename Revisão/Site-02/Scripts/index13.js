3c682f54af2dc2d7e785fe52406e311a6b7c6b85
Rafael Paz <rafael_mourapaz@hotmail.com>
2026-01-18 16:36:00 -0300

    exercicios

--- Revisão/Site-02/Scripts/index13.js
+++ Revisão/Site-02/Scripts/index13.js
@@ -8,3 +8,27 @@ let username = "Rafael Paz";
 let latters = [...username];
 
 console.log(latters);
+
+
+function sum(...numbers) {
+    let result = 0;
+    for(let number of numbers) {
+        result += number;
+    }
+
+    return result;
+}
+
+function getAverger(...numbers) {
+    let result = 0;
+    for(let number of numbers) {
+        result += number;
+    }
+
+    return result / numbers.length;
+}
+
+
+const total = getAverger(75, 100, 85, 90, 50);
+
+console.log(total);
---
+++ Revisão/Site-02/Scripts/index15.js
@@ -0,0 +1,40 @@
+function generatePassword(length, includeLowerCase, includeUperCase,includeNumber, includeSymbols ) {
+
+    const lowerCaseChars ='abcdefghijklmnopqrstuvwxyz';
+    const upPerCaseChars ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numberChars = '1234567890';
+    const symbosChars = '!@#$%¨&*()-+=_';
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowerCase ? lowerCaseChars : "";
+    allowedChars += includeUperCase ? upPerCaseChars : "";
+    allowedChars += includeNumber ? numberChars : "";
+    allowedChars += includeSymbols ? symbosChars : "";
+
+    if(length <= 0) {
+        return `(password length must be at less 1)`;
+    }
+    if(allowedChars.length === 0){
+        return `(At list 1 set of characteres needs to be selected)`;
+    }
+
+    for(let i = 0; i < length; i++) {
+        const randonIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randonIndex];
+    }
+
+    return password;
+}
+
+const passwordLength = 12;
+const includeLowerCase = true;
+const includeUperCase = true;
+const includeNumber = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowerCase, includeUperCase, includeNumber, includeSymbols);
+
+document.getElementById('text').textContent = password;
+console.log(`Password: ${password}`);
