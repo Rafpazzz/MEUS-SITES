00168b3e316b140a657a2efb366ff708f3a6eb04
Rafpazzz <rafael_mourapaz@hotmail.com>
2026-02-20 21:33:22 -0300

    aprendendo a construir a estrutura do projeto React

--- site-003/src/main.jsx
+++ site-003/src/main.jsx
@@ -1,30 +1,23 @@
 import React, { StrictMode } from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 
-//guardar a cache dos dados
+//Intouito desse arquivo é prover o contexto de navegção da aplicação
+
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// manter meu cliente em cache
 const queryClient = new QueryClient();
-
-
-
-//variavel que recebe um array de rotas, cada rota é representada por {}
-const router = createBrowserRouter([
-  //rota 1
-  {
-    path: "/",
-    element: <App />
-  },
-  
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* garante que a aplicação do query CLiente funcione em qualquer lugar */}
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )
